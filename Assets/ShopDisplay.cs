76f9b074deb4ce140df4f714d9ad0fb984dfc7fe
Daniel <dancoches@gmail.com>
2021-10-19 15:32:15 -0500

    Shop Works!

--- Assets/ShopDisplay.cs
+++ Assets/ShopDisplay.cs
@@ -8,41 +8,45 @@ public class ShopDisplay : MonoBehaviour
 {
     [SerializeField] private GameObject ImageObject;
     [SerializeField] private RectTransform ScrollPanel;
-    [SerializeField] private Transform Viewport;
+    [SerializeField] private RectTransform ContentPanel;
+    [SerializeField] private Transform WhereToPlaceItems;
     public List<ItemData> itemsList = new List<ItemData>();
     public Text[] textToReplace;
     public Image[] imagesToReplace;
+    public int tileSize = 5;
 
     public ShopCell[,] Grid;
 
     private void Awake()
     {
+        int drawCount = itemsList.Count;
         Utils.Vertical = Mathf.FloorToInt(ScrollPanel.sizeDelta.y);
         Utils.Horizontal = Mathf.FloorToInt(ScrollPanel.sizeDelta.x);
-        Utils.Columns = Mathf.Min(4, itemsList.Count);
-        Utils.Rows = Mathf.Min(4, itemsList.Count);
-        Grid = new ShopCell[Utils.Columns, Utils.Rows];
+        Utils.Columns = Mathf.Min(4, drawCount);
+        Utils.Rows = Mathf.CeilToInt(drawCount/4.0f);
+        Grid = new ShopCell[Utils.Rows, Utils.Columns];
 
-        Debug.Log(Utils.Rows);
-
-        for (int i = 0; i < Utils.Columns; i++)
+       
+        for (int i = 0; i < Utils.Rows; i++)
         {
-            //Debug.Log("Epic");
-            for (int j = 0; j < Utils.Rows; j++)
+            
+            for (int j = 0; j < Utils.Columns && i * Utils.Columns + j < drawCount; j++)
             {
 
-                //GameObject imageObj = Instantiate(ImageObject) as GameObject;
-                //GameObject imageObj = Instantiate(ImageObject, Utils.GridToWorldPosition(i, j), Quaternion.identity);
+                
+                float posX = j * tileSize - 100;
+                float posY = i * -tileSize + (95 * Utils.Rows);
+                Grid[i, j] = new ShopCell(ImageObject, 1, 1 );
+                Grid[i,j].gameObject.transform.SetParent(WhereToPlaceItems.transform, false);
 
-                Grid[i, j] = new ShopCell(ImageObject, Utils.Horizontal/2 + (i*300), Utils.Vertical/2* j );
-
-
-                Grid[i,j].gameObject.transform.SetParent(Viewport.transform, false);
+                Grid[i, j].gameObject.transform.position = new Vector2(posX, posY);
 
 
 
             }
         }
+
+        ContentPanel.sizeDelta = new Vector2(ContentPanel.sizeDelta.x, ContentPanel.sizeDelta.y * Utils.Rows);
 
     }
 
@@ -55,11 +59,13 @@ public class ShopDisplay : MonoBehaviour
 
         textToReplace = FindObjectsOfType<Text>().Where(obj => obj.name == "Item_txt").ToArray<Text>();
         imagesToReplace = FindObjectsOfType<Image>().Where(obj => obj.name == "ItemImage").ToArray<Image>();
+        Debug.Log(imagesToReplace.Length);
 
          for (int i = 0; i < itemsList.Count; i++)
         {
 
             Debug.Log(itemsList[i]);
+            Debug.Log(i);
             Debug.Log(imagesToReplace[i]);
             textToReplace[i].text = itemsList[i]._name;
             imagesToReplace[i].sprite = itemsList[i].visualSprite;
